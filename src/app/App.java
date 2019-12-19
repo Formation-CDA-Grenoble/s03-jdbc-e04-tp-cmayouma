e3406e8065d9d6dcdf7c42ee28c5da54e8d2477d
Naim391 <54673303+Naim391@users.noreply.github.com>
2019-12-19 16:18:20 +0100

    requête 3 faite

--- src/app/App.java
+++ src/app/App.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class App {
     public static void main(String[] args) throws Exception {
         ResultSet users = DBManager.sendQuery(
-            "SELECT * FROM `user`"
+            "SELECT count(*) FROM `product` WHERE brand_id=1"
         );
 
         while (users.next()) {
